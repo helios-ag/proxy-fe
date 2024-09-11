0c01248ed8374911a8070508eda0f8c88237c37a
Al Ganiev <helios.ag@gmail.com>
2024-09-11 23:05:08 +1000

    use docker host names

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import "time"
+
+const (
+	CacheAuthorArticles   = 2 * time.Minute
+	DetailedArticledCache = 5 * time.Minute
+	CacheAuthorsList      = 5 * time.Minute
+)
+
 type Config struct {
 	DB *DBConfig
 }
@@ -12,7 +20,7 @@ type DBConfig struct {
 func GetConfig() *Config {
 	return &Config{
 		DB: &DBConfig{
-			Host: "127.0.0.1",
+			Host: "proxy-service-db.local",
 			Port: 6379,
 		},
 	}
